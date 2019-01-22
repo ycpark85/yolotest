a4454d97f7609fe9354272eb77a1768e4326ccf3
ycpark85 <ycpark85@gmail.com>
2019-01-23 06:45:02 +0900

    제발좀 되라

--- pages/users/game.vue
+++ pages/users/game.vue
@@ -1,21 +1,18 @@
 <template>
 
-    <v-container class="con elevation-1 con-1" v-if="img3==''">
+    <v-container class="con" v-if="img3==''">
         <v-layout align-space-between justify-center column fill-height >
                <v-flex xs12 >
                     <v-card flat>
-                        <v-card-title class="ques-1">선호하는 활동을 선택하세요.</v-card-title>
-                        <v-card-title class="ques-1-1">모든 선택을 완료하시면 최종친구를 추천해드립니다.</v-card-title>
+                        <v-card-title class="ques-1">Q1.이상형은?</v-card-title>
                     </v-card>
                 </v-flex>
-                <v-flex xs12 class="que-img" mt-3>
-                    <div class="gameimg">   
+                <v-flex xs12 class="que-img" mt-4>
                     <img :src="img1" alt="" srcset="" @click="sel">
-                    </div>
                 </v-flex>
                 <v-flex xs12 class="ques-2" align-center>
                 <v-card flat class="ques-2">
-                        {{text1}}         
+                               
                 </v-card>
                  </v-flex>
                 <v-flex xs12 class="que-img" mt-3>
@@ -23,7 +20,7 @@
                 </v-flex>
                 <v-flex xs12 class="ques-2" align-center>
                 <v-card flat class="ques-2">
-                       {{text2}}       
+                            
                 </v-card>
                  </v-flex>
         
@@ -46,7 +43,8 @@
               <v-flex xs12>
                      <v-card>
                         <v-card-title>
-                            {{text3}}
+                            당신에게 어울리는 여행지는 ***입니다.
+                             추천된 친구들과 친해지고 함께 여행지로 놀러가 보세요.
                         </v-card-title>
                     </v-card>
             </v-flex>
@@ -76,30 +74,21 @@ export default {
             img1:'',
             img2:'',
             img3:'',
-            semple:['0','실외활동','실내활동','자연활동','도시활동','활동적인','섬세한','여행','레저스포츠','파티',
-            '워크아웃', '음악','운동','공방체험','요리체험','맛집탐방','명소탐방','수상스포츠','산악스포츠','컨셉파티','공연','라이딩','구기운동','악기','댄스','이종격투기','헬스','인테리어','케어용품','메인디쉬','디저트','맛집탐방을 선호하는 당신에게 친구와 콘텐츠를 추천해 드립니다.','관광명소를 선호하시는 당신에게 친구들을 추천드립니다.','해양스포츠를 선택한 당신에게 함께할 친구분들을 추천해 드려요.','산악스포츠를 선택한 당신에게 다음과 같은 친구분들을 추천드려요.','컨셉파티를 선택한 당신에게 욜로모여 협력업체인 솔게스트하우스와 함께하실 친구분들을 추천드려요.','공연을 선택하신 당신에게 함께 공연을 보러갈 친구를 추천드릴게요.','라이딩을 선택한 당신에게 함께 라이딩 할 친구분을 추천드릴게요','구기운동을 선택한 당신에게 함께 운동을 즐길 친구를 추천해 드릴게요.','악기연주를 선택한 당신에게 함께 밴드를 꾸릴수 있게 친구를 추천드립니다.','댄스를 선택한 당신에게 함께 춤출 파트너를 소개해 드릴게요.','이종격투기를 선택한 당신에게 스파링 파트너를 추천해드리요','헬스를 선택한 당신에게 쉽게 포기 할수 없도록 동료를 추천해드릴게요.','인테리어를 선택한 당신에게 컨텐츠와 함께할 친구를 추천드려요','케어용품을 선텍한 당신에게 함께 할 친구를 소개합니다.','메인디쉬를 선택한 당신에게 함께 식사를 할 친구분들을 추천 드릴게요','디저트를 선택한 당신에게 달달한 친구들을 추천드릴게요.'],
-
             gamesel:[],
             numsel:[],
-            items: [{}],
-            text1:'',
-            text2:'',
-            text3:'',
-
+            items: [{}]
         }
     },
     methods:{
         gameimg(){
 
             for(var i=1; i<=46; i++){
-                var imgtx='../../uploads/'+i+'.png'
-                this.gamesel[i] = imgtx
-                
+                var imgtx='../../uploads/'+i+'.jpg'
+                this.gamesel.push(imgtx)
             }
-             this.img1=this.gamesel[1]
-             this.text1 = this.semple[1]
+            this.img1=this.gamesel[1]
              this.img2=this.gamesel[2]
-             this.text2=this.semple[2]   
+            
            
           
             
@@ -117,13 +106,11 @@ export default {
                    
                   if(i<15){
                    this.img1=this.gamesel[i*2+1]
-                   this.text1 = this.semple[i*2+1]
                    this.img2=this.gamesel[i*2+2]
-                    this.text2=this.semple[i*2+2]  
+                   
                   }else if(i>=15){
 
                     this.img3=this.gamesel[i+16]
-                    this.text3=this.semple[i+16]
                     this.matnum = i+16 
                     this.save()
                    
@@ -170,13 +157,8 @@ export default {
     margin-bottom:30px;
 }
 .ques-1{
-    margin-top: 10px;
-    font-size: 15pt;
+    font-size: 18pt;
     font-weight: bold;
-}
-.ques-1-1{
-    font-weight: bold;
-    color: gray;
 }
 .ques-2{
     text-align: center;
@@ -188,9 +170,5 @@ export default {
     min-width: 350px;
     height: 150px;
 }
-.gameimg{
-    
-}
-img{
-width: 100%;}
+img{width: 100%;}
 </style>
